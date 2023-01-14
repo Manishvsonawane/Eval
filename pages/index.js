d871e82017ab41f205f6c6663e969f4a1750b181
Manish Vijay Sonawane <sonawanemanish349@gmail.com>
2023-01-14 22:01:48 +0530

    eval3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {Link} from "react-router-dom";
 import { Fresca, Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import { getImageSize } from 'next/dist/server/image-optimizer'
@@ -7,7 +8,7 @@ import { URLSearchParams } from 'next/dist/compiled/@edge-runtime/primitives/url
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({github}) {
   return (
     <>
     <div>
@@ -17,26 +18,27 @@ export default function Home() {
               height:"85px",
               border:"1px solid grey",
               display:"flex",
+              fontWeight:"bold",
               justifycontent: "space-around",
               alignItems: "center",
-            
             }}
           >
             <p>Manish Sonawane</p>
             <p>Projects</p>
             <p>Education</p>
           </div>
-          <div style={{ width:"22%", height:"750%", border:"1px solid white"}}>
+          <div style={{ width:"22%", height:"550px", border:"1px solid teal"}}>
             <img
-            src={git.avatar_url}
+            src={github.avatar_url}
             alt="img"
             width={"230px"}
             style={{ borderRadius: "35px" }}
           />
-          <h2>{git.name}</h2>
-          <p style={{ color: "red" }}>{git.login}</p>
+          <h1>{github.name}</h1>
+          <br/>
+          <p style={{ color: "red" }}>{github.login}</p>
           <br />
-          <p>{git.bio}</p>
+          <p>{github.bio}</p>
           <button>
           <Link href= "https://drive.google.com/file/d/1zZbTuek8GcLOoQm2Wz6c9mZiH5B6pM2g/view?usp=sharing"> 
             Resume
@@ -47,39 +49,40 @@ export default function Home() {
              
          </button>
         </div>
-        <div style={{display:"grid",width:"20%",gridTemplateColumns:"respet(3,1fr)", border:"1px solid white"}}>
+        <div style={{display:"grid",width:"22%",gridTemplateColumns:"respet(3,1fr)", border:"1px solid teal"}}>
           <p>HTML</p>
           <p>CSS</p>
           <p>Javascript</p>
           <p>React</p>
-          <p>Typescript</p>
-          <p>Next.JS</p>
           <p>NodeJS</p>
           <p>MongoDB</p>
           <p>Git</p>
+          <p>Typescript</p>
+          <p>Next.JS</p>
+          
 
 
       </div>
-      <div style={{display:"grid",gridTemplateRows:"repeat(3,1fr)",width:"20%",border:"1px solid white"}}>
+      <div style={{width:"20%",height:"auto",display:"grid",gridTemplateRows:"repeat(3,1fr)",border:"1px solid teal"}}>
       </div>
       </div>
 
-
-      
       </>
       );
    } 
    
    export async function getServerSideProps() {
+
+   try {
     const Mdata = await fetch("https://api.github.com/users/Manishvsonawane")
     const mans = await Mdata.json();
-    console.log(mans);
+    // console.log(mans);
     return{
-      props: {
-
-        git: mans,
-      },
+      props: {github: mans},
     };
+   } catch (error) {
+    console.log(error);
+   }
    }
 
 
--- pages/project.jsx
+++ pages/project.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
 
-const projects = ({projects}) => {
+const project = ({manish}) => {
   return (
     <div>
-        {projects.map((el,inx)=>(
+        <h1>PROJECTS</h1>
+        <br/>
+        <br/>
+        {manish.map((el)=>(
             <div key={el.id}>
-                <p>{el.full_name}</p>
-                <p>{el.language}</p>
-                <p>{el.stargaxers_count}</p>
-                <p>{el.watchers_count}</p>
-                <p>{el.visibility}</p>
+                <h4>{el.full_name}</h4>
+                <h4>{el.language}</h4>
             </div>
         ))}
     </div>
   )
 }
 
-export async function staticp() {
+export async function getStaticProps() {
+
+   try {
     const data = await fetch(`https://api.github.com/search/repositories?q=user:Manishvsonawane+fork:true&sort=update&per_page=10&type=Repositories`)
     const response = await data.json();
-
+    console.log(response);
     return {
-        props:{
-            projects:response.items,
-        },
+        props:{manish:response.item},
     }
+   } catch (error) {
+    console.log(error);
+   }
 }
-export default projects; 
+export default project; 
